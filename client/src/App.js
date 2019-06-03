dc2f2f6d71a7d4e6383179b7feb2428f2a528c22
Leah-Grace <leahgdg@hotmail.com>
2019-06-02 23:56:02 -0400

    rendered all buttons and included client-side javascript

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,8 @@ statue = {
     console.log(this.state);
   }
 //crate method to get user input
-getUserInfo = () => {
-
+getUserInfo = (user) => {
+console.log(`Get info for the user`);
 
 ///how to use e.target.value value to find users?!?!?!
 
@@ -30,7 +30,7 @@ getUserMessage = () => {
   return (
     <div className="App">
 <h2>My Express App</h2>
-<input onChange = {e => console.log(e.target.value)} placeholder="user name" type="text"></input><button onClick={this.getUserInfo}>Get user Info</button>
+<input onChange = {e => console.log(`${e.target.value}`)} placeholder="user name" type="text"></input><button onClick={this.getUserInfo}>Get user Info</button>
 
 <h5>User Registration</h5>
 <input
@@ -44,8 +44,11 @@ getUserMessage = () => {
 }
 placeholder="user massage" type="text" />
 <button onClick={this.getUserRegistration}>Submit</button>
+<h5>Get All Users</h5>
+<button onClick = {e => console.log(`Get all users`)}>Get All Users</button>
     </div>
   );
 }
 }
 export default App;
+ 
